677389b5ae8c84f051c1c48df689af7b90d939b1
Rahil <rahil.dedhia@students.olin.edu>
2015-03-13 03:56:11 -0400

    tryna make dis shit secure

--- public/js/remix.js
+++ public/js/remix.js
@@ -10,7 +10,7 @@ function createJRemixer(context, jquery, apiKey) {
         // If you have an EN TRack ID and the location of the audio.
         remixTrackById: function(trackID, trackURL, callback) {
             var track;
-            var url = 'http://developer.echonest.com/api/v4/track/profile?format=json&bucket=audio_summary'
+            var url = 'https://developer.echonest.com/api/v4/track/profile?format=json&bucket=audio_summary'
 
            var retryCount = 3;
            var retryInterval = 3000;
@@ -22,7 +22,7 @@ function createJRemixer(context, jquery, apiKey) {
                     
                     // This call is proxied through the yahoo query engine.  
                     // This is temporary, but works.
-                    $.getJSON("http://query.yahooapis.com/v1/public/yql", 
+                    $.getJSON("https://query.yahooapis.com/v1/public/yql", 
                         { q: "select * from json where url=\"" + analysisURL + "\"", format: "json"}, 
                         function(data) {
                             if (data.query.results != null) {
